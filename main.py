b6fd47a27f5d56d7f9bce6cfd0bd1e1b93e71762
sudipkarki123 <sudipkarkicoding@gmail.com>
2024-12-02 18:56:49 +0545

    lesson 2

---
+++ main.py
@@ -0,0 +1,41 @@
+# name=input("Enter your name:")
+# print("welcome to codingal",name) 
+
+# import keyword
+# print("list of keywords:\n")
+# print(keyword.kwlist)
+
+# a=int(input("Enter the number1:"))
+# b=int(input("Enter the number2:"))
+# print("Addition",a+b)
+# print("Difference",a-b)
+# print("Multiply",a*b)
+# print("Division",a/b)
+# print("Quotient",a//b)
+# print("Reminder",a%b)
+# print("Exponential",a+b)
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+
+# Eng=int(input("Enter English Marks:"))
+# Acc=int(input("Enter Account Marks:"))
+# Nep=int(input("Enter Nepali Marks:"))
+# Total=Eng+Acc+Nep
+# avg=(Eng+Acc+Nep)/3
+# print("total marks:",Total)
+# print("Average:",avg)
+
+# word=input("enter the word:")
+# print(word.upper())
+# print(word.lower())
+# print(word.capitalize())
+# print(word.swapcase())
+# print("No of letters in the word:",len(word))
+1
+a=int(input("Enter the number1:"))
+b=int(input("Enter the number2:"))
+c=int(input("Enter the number3:"))
+print("Addition",a+b+c)
+ 
